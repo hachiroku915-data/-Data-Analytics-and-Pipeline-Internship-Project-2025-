508f8438500893089f692e46cb7c2faaca6b9466
hachiroku915-data <hachiroku915@gmail.com>
2026-09-03 23:03:34 -0700

    Add files via upload

--- Json-Merger.py
+++ Json-Merger.py
@@ -1,11 +1,9 @@
-#Due to the company's database requiring every JSON packet for a 24 hour period to be downloaded individually, this script was designed to merge datapackets from a like branch together to make loading them into Power BI easier
-#It was also designed to make organization of files more streamlined
 import json
 import os
 
-inputFolder = r"D:\Quanty\Queries\Comfandi\Data Batches\Odontología Torres\Raw"
-outputFolder = r"D:\Quanty\Queries\Comfandi\Data Batches\Odontología Torres\Merged"
-fileNameOutput = "Odontología Torres_Merged.json" #Current Branch being worked on
+inputFolder = "D:\Quanty\Queries\Comfandi\Data Batches\Odontología Torres\Raw"
+outputFolder = "D:\Quanty\Queries\Comfandi\Data Batches\Odontología Torres\Merged"
+fileNameOutput = "Odontología Torres_Merged.json"
 
 os.makedirs(outputFolder, exist_ok=True)
 #Makes a new directory but checks if one already exists
@@ -14,7 +12,6 @@ outputFile = os.path.join(outputFolder, fileNameOutput)
 count = 0
 mergedData = []
 
-#Iterate through all JSON data packets
 for filename in os.listdir(inputFolder):
     filePath = os.path.join(inputFolder, filename)
     with open(filePath, 'r', encoding='utf-8') as f:
@@ -30,7 +27,7 @@ for filename in os.listdir(inputFolder):
 
 completeData = {
     "ticketsBatch":{
-        "BatchId": "Odontología Torres", 
+        "BatchId": "Clínica Cartago Ambulatorio",
         "tickets": mergedData
     }
 }
@@ -39,5 +36,5 @@ with open(outputFile, 'w', encoding='utf-8') as out_file:
     print ("Saving to file")
     json.dump(completeData, out_file, ensure_ascii=False, indent=4)
     
-print(f"Merged{len(mergedData)} items into {outputFile}") #Informs user of successful output
+print(f"Merged{len(mergedData)} items into {outputFile}")
     
