2670af4caffd81254ce295e2dbf4d731879ecefa
hachiroku915-data <hachiroku915@gmail.com>
2026-09-03 23:03:16 -0700

    Add files via upload

--- Auto Query Version 4.0.py
+++ Auto Query Version 4.0.py
@@ -1,6 +1,3 @@
-#Please note that some information contained within the original version of this script was sensitive company information as it was designed to work specifically with thier system.
-#Due to this issue, I have REDACTED sensitive information and made note of so below. The structure of the script remains the same.
-#This program was originally constructed and refined between Aug. and Jun. of 2025
 import json
 import requests
 import pytz
@@ -10,13 +7,15 @@ import time
 import os
 from playsound import playsound
 
-targ_timezone = pytz.timezone("Iceland") #Allows us to get the correct timezone within this library. This is due to the company location and the way their internal system operates.
+targ_timezone = pytz.timezone("Iceland")
+
+#start with number 9
 
 day = 1
 numOfDays = 0
 
-branch_name = "" #This is the name of the specific medical office we are pulling data for
-branch = "" #This is where the specific token for the branch's data is held
+branch_name = ""
+branch = ""
 
 
 month = 1
@@ -28,55 +27,55 @@ count = int(input("Please enter a number for the branch to be selected: "))
 
 if count == 0:
     branch_name = "Odontología Torres"
-    branch = "REDACTED" #All of these branch variables would normally hold the specific token for the branch but have been removed for security reasons. 
+    branch = "oU4v5nt77uYfiaQn3cZQ"
 elif count == 1:
     branch_name = "Odontología Alameda"
-    branch = "REDACTED"
+    branch = "8bNXhoGfGKceVo9Nke1k"
 elif count == 2:
     branch_name = "IPS Yumbo"
-    branch = "REDACTED"
+    branch = "WRnpf0mRDVPePtsnzkz2"
 elif count == 3:
     branch_name = "IPS Tuluá"
-    branch = "REDACTED"
+    branch = "IzhIz4rNSclmALbX3bp7"
 elif count == 4:
     branch_name = "IPS Torres"
-    branch = "REDACTED"
+    branch = "juhBKQAUfukPcNpv3Crf"
 elif count == 5:
     branch_name = "IPS Tequendama"
-    branch = "REDACTED"
+    branch = "3X6VRYkJy4NQrBPkHXT6"
 elif count == 6:
     branch_name = "IPS SIA Sur"
-    branch = "REDACTED"
+    branch = "dzbkwGsOGHxE1YgDcO41"
 elif count == 7:
     branch_name = "IPS SIA Norte"
-    branch = "REDACTED"
+    branch = "BHZy2g4sEcbkYaaIr8qk"
 elif count == 8:
     branch_name = "IPS San Nicolás"
-    branch = "REDACTED"
+    branch = "33sZlGOD9jz3dd1lhkJP"
 elif count == 9:
     branch_name = "IPS Prado"
-    branch = "REDACTED"
+    branch = "XsEZLZwWPPEIEuVTBGTY"
 elif count == 10:
     branch_name = "IPS Palmira"
-    branch = "REDACTED"
+    branch = "Q16lIZ660RUwNkpPQD4a"
 elif count == 11:
     branch_name = "IPS Morichal"
-    branch = "REDACTED"
+    branch = "d1G2sZQ6H3xgPQKYxTta"
 elif count == 12:
     branch_name = "IPS Cartago"
-    branch = "REDACTED"
+    branch = "ax0GGBWvdHcmSLuCfrlk"
 elif count == 13:
     branch_name = "IPS Buga"
-    branch = "REDACTED"
+    branch = "GQxoTDvc0QNWRCU0tXSU"
 elif count == 14:
     branch_name = "IPS Alameda"
-    branch = "REDACTED"
+    branch = "5EZJywAysVflvI1u9A2o"
 elif count == 15:
     branch_name = "Clínica Cartago Hospitalario"
-    branch = "REDACTED"
+    branch = "5tWVAeM0IRPN3AJSnXa2"
 elif count == 16:
     branch_name = "Clínica Cartago Ambulatorio"
-    branch = "REDACTED"
+    branch = "6evy2NDnWmAI6WSOx0Cc"
 else:
     branch = "" 
     branch_name = ""
@@ -88,8 +87,8 @@ while (year <= 2025):
         if month == 1:
             numOfDays = 31
             monthName = "January"
-        elif month == 2: 
-            if (year % 4 == 0): #Automatically calculates if the current year we are pulling data from is a leap year and accounts for the extra day. 
+        elif month == 2:
+            if (year % 4 == 0):
                 if (year % 100 != 0):
                     numOfDays = 29
                 else:
@@ -133,7 +132,7 @@ while (year <= 2025):
         else:
             print ("This is an invalid month, please enter a valid month: \n" )
 
-        output_location = f'D:\Quanty\Queries\Comfandi\Complete List' #Specified file path for this specific dataset, would be changed based on the needs of the company. This version is hardcoded due to the fact that the location of the data I was downloading was constant.
+        output_location = f'D:\Quanty\Queries\Comfandi\Complete List'
 
 
 
@@ -154,14 +153,14 @@ while (year <= 2025):
 
 
 
-            url = "REDACTED" #Holds specific api URL for data extraction.
+            url = "https://qanty.com/api/tickets_detailed"
             headers = {
                 'Content-Type': 'application/json',
-                'Authorization': 'REDACTED' #Holds the authorization token for the company's database. 
+                'Authorization': 'fBUURJZ94p3Q14YV80Vp9D9Mt4ZQhbr5'
             }
 
             body = {
-                "company_id": "REDACTED",  
+                "company_id": "qWBRkJQkaSBOc0d52c8E",  
                 "branch_id": branch,
                 "start_date": startEpoch,
                 "end_date": endEpoch
@@ -182,7 +181,7 @@ while (year <= 2025):
                     "Sede": ticket.get("branchName"),
                     "Línea actual": ticket.get("currentLineName"),
                     "Línea inicial": ticket.get("startLineName"),
-                    "Fecha del Turno": datetime.utcfromtimestamp(ticket.get("issueDate", 0) / 1000).isoformat(), #Depriciated format has been left unchanged to preserve originality. The modern equvilant of this line would be: "Fecha del Turno": datetime.fromtimestamp(ticket.get("issueDate", 0) / 1000, tz=UTC)
+                    "Fecha del Turno": datetime.utcfromtimestamp(ticket.get("issueDate", 0) / 1000).isoformat(),
                     "CustomerName": customer.get("name"),
                     "CustomerLastName": customer.get("lastName"),
                     "CustomerDocId": customer.get("docId"),
@@ -192,7 +191,7 @@ while (year <= 2025):
 
             
 
-            fileName = f"{branch_name}_{month} {day} {year}.json" #Automatically names the file, files had to be downloaded individually due to the constraints of the company database. A seperate merging program was created to resolve this.
+            fileName = f"{branch_name}_{month} {day} {year}.json"
 
             output_file = os.path.join(output_location, fileName) if output_location else fileName
 
@@ -206,7 +205,7 @@ while (year <= 2025):
     month = 1
 
 completeSound = "D:\Quanty\Auto Downloader\complete.wav"
-playsound (completeSound) #Automatically plays a notification sound to alert me when the download had finished. This allowed me to focus on other aspects of the project while this worked in the background.
+playsound (completeSound)
 
 
 
